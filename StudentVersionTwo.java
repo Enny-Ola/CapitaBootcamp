e89f25f84c2b680a0393963cf2d8e83681d34c24
Eniola <115539547+Enny-Ola@users.noreply.github.com>
2023-01-20 17:17:13 +0000

    Update StudentVersionTwo.java
    Updating the code following debugging using variable watching technique and compiler warning technique on IntelliJ IDEA

--- StudentVersionTwo.java
+++ StudentVersionTwo.java
@@ -1,108 +1,95 @@
 import java.util.Scanner;
-    public class StudentVersionTwo {
+
+public class StudentVersionThree {
     public static void main(String[] args) {
         boolean run = true;
         String choice;
+
         do {
             choice = displayMenu();
-            if (choice.equals("x")){
-                run = false;
+            switch (choice) {
+                case "x" -> run = false;
+                case "1" -> {
+                    double x = getNumber(1);
+                    double y = getNumber(2);
+                    double total = sumMethod(x, y);
+                    display(total);
+                    run = checkFinish();
+                }
+                case "2" -> {
+                    double x = getNumber(1);
+                    double y = getNumber(2);
+                    double total = subtractMethod(x, y);
+                    display(total);
+                    run = checkFinish();
+                }
+                case "3" -> {
+                    double x = getNumber(1);
+                    double y = getNumber(2);
+                    double total = multiplyMethod(x, y);
+                    display(total);
+                    run = checkFinish();
+                }
+                case "4" -> {
+                    double x = getNumber(1);
+                    double y = getNumber(2);
+                    double total = divideMethod(x, y);
+                    display(total);
+                    run = checkFinish();
+                }
+                default -> System.out.println("Entry not recognised, please try again...");
             }
-            else if (choice.equals("1")){
-                int x = getNumber(1);
-                int y = getNumber(2);
-                int total = sumMethod(x,y);
-                display(total);
-                run = checkFinish();
-            }
-            else if (choice.equals("2")){
-                int x = getNumber(1);
-                int y = getNumber(2);
-                int total = subtractMethod(x,y);
-                display(total);
-                run = checkFinish();
-            }
-            else if (choice.equals("3")){
-                int x = getNumber(1);
-                int y = getNumber(2);
-                int total = multiplyMethod(x,y);
-                display(total);
-                run = checkFinish();
-            }
-            else if (choice.equals("4")){
-                int x = getNumber(1);
-                int y = getNumber(2);
-                int total = divideMethod(x,y);
-                display(total);
-                run = checkFinish();
-            }
-            else {
-                System.out.println("Entry not recognised, please try again...");
-            }
-        } while (run) ;
+        } while (run);
     }
-    public static int getNumber(int count){
-        boolean numberWrong = true;
-        while (numberWrong) {
+    public static double getNumber(int count){
             if (count == 1) {
                 System.out.print("Enter 1st number: ");
             } else {
-                System.out.println("Enter 2nd number: ");
+                System.out.print("Enter 2nd number: ");
             }
             Scanner reader = new Scanner(System.in);
             try {
-                    int x = reader.nextInt();
-                    return x;
-                } catch (Exception e){
-                    System.out.println("Number not recognised, please try again.");
-                }
-        }
+                return reader.nextDouble();
+            } catch (Exception e){
+                System.out.println("Number not recognised, please try again.");
+            }
         return 0;
     }
-        public static String displayMenu(){
-            System.out.println(" Calculator Menu");
-            System.out.println("1. Add numbers");
-            System.out.println("2. Subtract numbers");
-            System.out.println("3. Multiply numbers");
-            System.out.println("4. Divide numbers");
-            System.out.println("x. Exit program");
-            System.out.println();
-            Scanner reader = new Scanner(System.in);
-            System.out.print("Enter option (1,2,3,4,x): ");
-            return reader.nextLine();
+    public static String displayMenu(){
+        System.out.println(" Calculator Menu");
+        System.out.println("1. Add numbers");
+        System.out.println("2. Subtract numbers");
+        System.out.println("3. Multiply numbers");
+        System.out.println("4. Divide numbers");
+        System.out.println("x. Exit program");
+        System.out.println();
+        Scanner reader = new Scanner(System.in);
+        System.out.print("Enter option (1,2,3,4,x): ");
+        return reader.nextLine();
     }
-        public static boolean checkFinish(){
-            Scanner reader = new Scanner(System.in);
-            System.out.print("Have you finished (y/n): ");
-            String ans = reader.nextLine().trim().toLowerCase();
-            if (ans.equals("y")){
-                return false;
-            }
-            else{
-                return true;
-            }
+    public static boolean checkFinish(){
+        Scanner reader = new Scanner(System.in);
+        System.out.print("Have you finished (y/n): ");
+        String ans = reader.nextLine().trim().toLowerCase();
+        return !ans.equals("y");
     }
-        public static int sumMethod(int n, int m){
-            System.out.println("When adding the numbers");
-            return (n * m);
-        }
-    
-        public static int subtractMethod(int n, int m){
-            System.out.println("When subtracting the numbers");
-            return (n - m);
-        }
-    
-        public static int multiplyMethod(int n, int m){
-            System.out.println("When multiplying the numbers");
-            return (n * m);
-        }
-    
-        public static int divideMethod(int n, int m){
-            System.out.println("When dividing the numbers");
-            return ((int)n / m);
-        }
-    
-        public static void display(int sum){
-            System.out.println("The answer is: " + sum);
-        }
+    public static double sumMethod(double n, double m){
+        System.out.println("When adding the numbers");
+        return (n * m);
     }
+    public static double subtractMethod(double n, double m){
+        System.out.println("When subtracting the numbers");
+        return (n - m);
+    }
+    public static double multiplyMethod(double n, double m){
+        System.out.println("When multiplying the numbers");
+        return (n * m);
+    }
+    public static double divideMethod(double n, double m){
+        System.out.println("When dividing the numbers");
+        return (n / m);
+    }
+    public static void display(double sum){
+        System.out.println("The answer is: " + sum);
+    }
+}
